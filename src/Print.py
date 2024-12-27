b66f4e3d7fd6c25b37d9414154d93c79c5082e16
ramakrishnareddy0490 <ramakrishnareddy0490@gmail.com>
2024-12-27 14:11:45 +0530

    Printing data n the terminal

---
+++ src/Print.py
@@ -0,0 +1,12 @@
+# Print the data in single line
+print('Hello World!') 
+
+# print data in multiple lines
+print("""\
+... Usage: thingy [OPTIONS]
+...      -h                        Display this usage message
+...      -H hostname               Hostname to connect to
+... """)
+
+# using escape(\) sequence
+print('I\'m a python developer')
